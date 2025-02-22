cfb03b9cfdbb62103c8fd44e57085ea9e24d411f
Ayeza Bashir <ayezabashir46@gmail.com>
2025-02-22 17:55:49 +0500

    screen one

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+const file_input = document.getElementById("file_input");
+const custom_file_input = document.querySelector(".custom__input__file");
+
+custom_file_input.addEventListener("click", ()=>{
+    file_input.click()
+})
