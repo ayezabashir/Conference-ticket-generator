58ffb9a008bd13c61608a5cc6b72dd7ce90c9603
Ayeza Bashir <ayezabashir46@gmail.com>
2025-02-24 23:58:34 +0500

    changes

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,15 @@
 const file_input = document.getElementById("file_input");
-const custom_file_input = document.querySelector(".custom__input__file .upload__image");
+const custom_file_input = document.querySelector(
+  ".custom__input__file .upload__image"
+);
 const img_input = document.querySelector(".upload__image");
 const view__img_container = document.querySelector(".view__image");
 const upload__image = document.querySelector(".upload__image");
 const remove_img = document.querySelector(".remove_img");
 const change_img = document.querySelector(".change_img");
+const generate_ticket = document.querySelector(".btn-ticket");
+const ticket_container = document.querySelector(".ticket__container");
+const form = document.querySelector(".form");
 var uploaded_image = "";
 
 custom_file_input.addEventListener("click", () => {
@@ -12,8 +17,8 @@ custom_file_input.addEventListener("click", () => {
 });
 
 file_input.addEventListener("change", () => {
-    view__img_container.style.display = "flex";
-    img_input.style.display = "none";
+  view__img_container.style.display = "flex";
+  img_input.style.display = "none";
   const reader = new FileReader();
   reader.addEventListener("load", () => {
     uploaded_image = reader.result;
@@ -23,11 +28,19 @@ file_input.addEventListener("change", () => {
   reader.readAsDataURL(file_input.files[0]);
 });
 
-remove_img.addEventListener("click", ()=>{
-    img_input.style.display = "flex";
-    view__img_container.style.display = "none";
-})
+remove_img.addEventListener("click", () => {
+  img_input.style.display = "flex";
+  view__img_container.style.display = "none";
+});
 
-change_img.addEventListener("click",()=>{
-    file_input.click()
-})
+change_img.addEventListener("click", () => {
+  file_input.click();
+});
+
+generate_ticket.addEventListener("click", () => {
+  const user_name = document.getElementById("u-name");
+  const user_github = document.getElementById("u-github");
+  const user_email = document.getElementById("u-email");
+  form.classList.add("d-none");
+  ticket_container.style.display = "black";
+});
