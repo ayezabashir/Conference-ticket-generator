2decd004de82c4f883a1b623f3cf34f733ed9d4d
Ayeza Bashir <ayezabashir46@gmail.com>
2025-02-26 22:36:11 +0500

    ticket

--- assets/main.js
+++ assets/main.js
@@ -10,6 +10,8 @@ const change_img = document.querySelector(".change_img");
 const generate_ticket = document.querySelector(".btn-ticket");
 const ticket_container = document.querySelector(".ticket__container");
 const form = document.querySelector(".form");
+const error_msg = document.querySelector(".error");
+const info_icon = document.getElementById("info_icon");
 var uploaded_image = "";
 
 custom_file_input.addEventListener("click", () => {
@@ -17,13 +19,22 @@ custom_file_input.addEventListener("click", () => {
 });
 
 file_input.addEventListener("change", () => {
-  view__img_container.style.display = "flex";
-  img_input.style.display = "none";
   const reader = new FileReader();
   reader.addEventListener("load", () => {
     uploaded_image = reader.result;
-    var view__image = document.querySelector(".view__image .image");
-    view__image.style.backgroundImage = `url("${uploaded_image}")`;
+    if(file_input.files[0].size > 500000){
+      error_msg.innerText = "File too large. Please upload a photo under 500KB";
+      error_msg.style.color ="red";
+      view__img_container.style.display = "none";
+      img_input.style.display = "flex";
+    }else{
+      error_msg.innerText = "Upload your photo (JPG or PNG, max size: 500KB).";
+      error_msg.style.color ="#8784a4";
+      view__img_container.style.display = "flex";
+      img_input.style.display = "none";
+      var view__image = document.querySelector(".view__image .image");
+      view__image.style.backgroundImage = `url("${uploaded_image}")`;
+    }
   });
   reader.readAsDataURL(file_input.files[0]);
 });
